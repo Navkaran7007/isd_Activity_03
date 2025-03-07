0152872009ff58c13ff5aacfcdea3426160431fb
Navkaran Singh Sidhu <nsidhu10@academic.rrc.ca>
2025-03-06 21:02:13 -0600

    completed payment_strategy file

--- patterns/strategy/payment_strategy.py
+++ patterns/strategy/payment_strategy.py
@@ -1,4 +1,21 @@
 """This module defines the PaymentStrategy class."""
 
-__author__ = ""
+__author__ = "Navkaran Singh Sidhu"
 __version__ = ""
+
+from abc import ABC, abstractmethod
+from billing_account.billing_account import BillingAccount
+from payee.payee import Payee
+
+
+class PaymentStrategy(ABC):
+    """
+    """
+    @abstractmethod
+    def process_payment(self, 
+                        account: BillingAccount, 
+                        payee: Payee, 
+                        amount: float)-> str:
+        """
+        """
+        pass
